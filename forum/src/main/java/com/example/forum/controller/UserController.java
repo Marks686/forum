8b685ec0f409e54cbbdd65be4d6dab770e78de4c
Marks686 <17829791810@163.com>
2024-05-19 14:51:10 +0800

    完成登录功能代码编写

--- forum/src/main/java/com/example/forum/controller/UserController.java
+++ forum/src/main/java/com/example/forum/controller/UserController.java
@@ -1,24 +1,27 @@
     package com.example.forum.controller;
 
-import com.example.forum.common.AppResult;
-import com.example.forum.common.ResultCode;
-import com.example.forum.model.User;
-import com.example.forum.services.IUserService;
-import com.example.forum.utils.MD5Util;
-import com.example.forum.utils.UUIDUtil;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import lombok.NonNull;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+    import com.example.forum.common.AppResult;
+    import com.example.forum.common.ResultCode;
+    import com.example.forum.config.AppConfig;
+    import com.example.forum.model.User;
+    import com.example.forum.services.IUserService;
+    import com.example.forum.utils.MD5Util;
+    import com.example.forum.utils.UUIDUtil;
+    import io.swagger.annotations.Api;
+    import io.swagger.annotations.ApiOperation;
+    import io.swagger.annotations.ApiParam;
+    import lombok.NonNull;
+    import lombok.extern.slf4j.Slf4j;
+    import org.springframework.web.bind.annotation.PostMapping;
+    import org.springframework.web.bind.annotation.RequestMapping;
+    import org.springframework.web.bind.annotation.RequestParam;
+    import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
+    import javax.annotation.Resource;
+    import javax.servlet.http.HttpServletRequest;
+    import javax.servlet.http.HttpSession;
 
-/**
+    /**
  * @Author 比特就业课
  */
 
@@ -80,5 +83,30 @@ public class UserController {
     }
 
 
+
+    /**
+     * 用户登录
+     * @param username 用户名
+     * @param password 密码
+     */
+    @ApiOperation("用户登录")
+    @PostMapping("/login")
+    public AppResult login (HttpServletRequest request,
+                            @ApiParam("用户名") @RequestParam("username") @NonNull String username,
+                            @ApiParam("密码") @RequestParam("password") @NonNull String password) {
+        // 1. 调用Service中的登录方法，返回User对象
+        User user = userService.login(username, password);
+        if (user == null) {
+            // 打印日志
+            log.warn(ResultCode.FAILED_LOGIN.toString());
+            // 返回结果
+            return AppResult.failed(ResultCode.FAILED_LOGIN);
+        }
+        // 2. 如果登录成功把User对象设置到Session作用域中
+        HttpSession session = request.getSession(true);
+        session.setAttribute(AppConfig.USER_SESSION, user);
+        // 3. 返回结果
+        return AppResult.success();
+    }
 
 }
