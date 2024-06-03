7e52f1b0459df79bd5db5968b67378f1cb9a1893
Marks686 <17829791810@163.com>
2024-06-03 20:53:55 +0800

    完成论坛项目上线

--- forum/src/main/java/com/example/forum/controller/TestController.java
+++ forum/src/main/java/com/example/forum/controller/TestController.java
@@ -1,54 +1,17 @@
 package com.example.forum.controller;
 
-import com.example.forum.common.AppResult;
-import com.example.forum.exception.ApplicationException;
-import com.example.forum.model.User;
-import io.swagger.annotations.*;
-import lombok.NonNull;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @Author 比特就业课
- */
-@Api(tags = "测试类的相关接口")
-//  表示返回的结果是数据
+// 表示返回的结果是数据
 @RestController
 // 定义一级映射路径
 @RequestMapping("/test")
 public class TestController {
 
-    @ApiOperation("测试接口1，显示你好Spring boot")
-    @GetMapping("/hello")
-    public String hello () {
-        return "hello, Spring boot...";
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello spring boot";
     }
 
-    @ApiOperation("测试接口4，按传入的姓名显示你好信息")
-    @PostMapping("/helloByName")
-    public AppResult helloByName (@ApiParam(value = "姓名") @RequestParam("name") @NonNull String name) {
-        return AppResult.success("hello : " + name);
-    }
-
-
-    @ApiOperation("测试接口2，显示抛出的异常信息")
-    @GetMapping("/exception")
-    public AppResult testException () throws Exception {
-        throw new Exception("这是一个Exception...");
-    }
-
-    @ApiOperation("测试接口2，显示抛出的自定义的异常信息")
-    @GetMapping("/appException")
-    public AppResult testApplicationException () {
-        throw new ApplicationException("这是一个ApplicationException...");
-    }
-
-    @ApiImplicitParams({
-            @ApiImplicitParam(value = "第一个参数", name = "first"),
-            @ApiImplicitParam(value = "用户信息", name = "user")
-    })
-    @ApiOperation("API参数测试")
-    @GetMapping("/testApi")
-    public AppResult testApiParams (String first, @RequestBody User user) {
-        return AppResult.success(first);
-    }
 }
--- forum/src/test/java/com/example/forum/ForumApplicationTests.java
+++ forum/src/test/java/com/example/forum/ForumApplicationTests.java
@@ -1,61 +1,15 @@
 package com.example.forum;
 
-import com.example.forum.dao.UserMapper;
-import com.example.forum.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.annotation.Resource;
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.UUID;
-
-
 @SpringBootTest
 class ForumApplicationTests {
-
-    // 数据源
-    @Resource
-    private DataSource dataSource;
-
-//     用户的Mapper
-    @Resource
-    private UserMapper userMapper;
-
-    @Test
-    void testConnection() throws SQLException {
-        System.out.println("dataSource = " + dataSource.getClass());
-        // 获取数据库连接
-        Connection connection = dataSource.getConnection();
-        System.out.println("connection = " + connection);
-        connection.close();
-    }
-
-    @Test
-    void testMybatis () {
-        User user = userMapper.selectByPrimaryKey(1l);
-//        System.out.println(user.getUsername());
-//        System.out.println(user.getArticleCount());
-//        System.out.println(user.getCreateTime());
-//        System.out.println(user.getAvatarUrl());
-//        System.out.println(user.getDeleteState());
-//        System.out.println(user.getEmail());
-//        System.out.println(user.getId());
-//        System.out.println(user.getIsAdmin());
-        System.out.println(user);
-
-    }
-
 
     @Test
     void contextLoads() {
         System.out.println("TEST: 基于Spring的论坛系统-前后端分离 ");
     }
 
-    @Test
-    void testUUID(){
-        System.out.println(UUID.randomUUID().toString());
-    }
 
 }
